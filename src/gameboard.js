bca499a9f89b1b545f4b2b8271ca40a1fb6017de
JonDm07 <jan.drmota1@gmail.com>
2023-11-29 19:50:47 +0100

    Change Gameboard factory to class

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,43 +1,41 @@
 import { Ship } from "./ship";
 import { convertPosition } from "./index";
 
-export function Gameboard() {
-  return {
-    // add start and end position
-    // check if position is not taken
-    placeShip: function (position, rotation, length) {
-      if (
-        rotation === undefined ||
-        rotation === null ||
-        position === undefined ||
-        position === null
-      ) {
+export class Gameboard {
+  // add start and end position
+  // check if position is not taken
+  placeShip(position, rotation, length) {
+    if (
+      rotation === undefined ||
+      rotation === null ||
+      position === undefined ||
+      position === null
+    ) {
+      return;
+    }
+
+    if (rotation === "x") {
+      const startPoint = convertPosition(position);
+
+      if (startPoint[1] > 9) {
         return;
       }
+    } else if (rotation === "y") {
+      const coords = convertPosition(position);
 
-      if (rotation === "x") {
-        const startPoint = convertPosition(position);
-
-        if (startPoint[1] > 9) {
-          return;
-        }
-      } else if (rotation === "y") {
-        const coords = convertPosition(position);
-
-        if (coords[0] > 9) {
-          return;
-        }
+      if (coords[0] > 9) {
+        return;
       }
+    }
 
-      if (!this.shipsPositions) {
-        this.shipsPositions = [];
-      }
+    if (!this.shipsPositions) {
+      this.shipsPositions = [];
+    }
 
-      const ship = Ship(length);
-      ship.position = position;
-      ship.rotation = rotation;
+    const ship = Ship(length);
+    ship.position = position;
+    ship.rotation = rotation;
 
-      this.shipsPositions.push(ship);
-    },
-  };
+    this.shipsPositions.push(ship);
+  }
 }
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -1,1 +1,6 @@
 import { Gameboard } from "./gameboard";
+
+test("create object", () => {
+  const obj = new Gameboard();
+  expect(obj instanceof Gameboard).toBe(true);
+});
